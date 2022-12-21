17576d521d819b67899cb8a39c68dcd98a01fa4b
cindy <1177031+cindywu@users.noreply.github.com>
2022-12-21 08:13:38 -1000

    hello world

---
+++ src/script.ts
@@ -0,0 +1,8 @@
+const statusDisplay : Element  = document.querySelector('.game--status')!;
+
+let playerX : boolean = true
+
+const thing: () => string = () => `hello world`
+
+statusDisplay.innerHTML = thing()
+
